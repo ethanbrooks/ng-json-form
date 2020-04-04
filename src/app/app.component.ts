33386cbcabe88a022c7cbb9cc3bdc85a8d74ec99
ethanbrooks <ebrooks@hotelherrera.com>
2020-04-04 01:24:48 -0500

    works with just schema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,52 +15,22 @@ export class AppComponent implements OnInit {
 
   layout = [ "*" ];
 
-  // layout = [
-  //   { "type": "flex", "flex-flow": "row wrap", "items": [ "first_name", "last_name" ] },
-  //   { "key": "address.street_1", "title": "Address", "placeholder": "Street" },
-  //   { "key": "address.street_2", "notitle": true },
-  //   { "type": "div",
-  //     "display": "flex",
-  //     "flex-direction": "row",
-  //     "items": [
-  //       { "key": "address.city", "flex": "3 3 150px",
-  //         "notitle": true, "placeholder": "City"
-  //       },
-  //       { "key": "address.state", "flex": "1 1 50px",
-  //         "notitle": true, "placeholder": "State"
-  //       },
-  //       { "key": "address.zip_code", "flex": "2 2 100px",
-  //         "notitle": true, "placeholder": "Zip Code"
-  //       }
-  //     ]
-  //   },
-  //   { "key": "birthday", "type": "date" },
-  //   { "key": "phone_numbers",
-  //     "type": "array",
-  //     "listItems": 3,
-  //     "items": [ {
-  //       "type": "div",
-  //       "displayFlex": true,
-  //       "flex-direction": "row",
-  //       "items": [
-  //         { "key": "phone_numbers[].type", "flex": "1 1 50px",
-  //           "notitle": true, "placeholder": "Type"
-  //         },
-  //         { "key": "phone_numbers[].number", "flex": "4 4 200px",
-  //           "notitle": true, "placeholder": "Phone Number"
-  //         }
-  //       ]
-  //     } ]
-  //   },
-  //   { "type": "section",
-  //     "title": "Notes",
-  //     "expandable": true,
-  //     "expanded": false,
-  //     "items": [ { "key": "notes", "type": "textarea", "notitle": true } ]
-  //   }
-  // ];
-
-  jsonSchema = {};
+  jsonSchema = {
+    "roomId" : {
+      "type" : "string",
+      "description" : "the id of the Conference Room"
+    },
+    "startDate" : {
+      "type" : "integer",
+      "format" : "int64",
+      "description" : "the start date of the Conference Room"
+    },
+    "endDate" : {
+      "type" : "integer",
+      "format" : "int64",
+      "description" : "the end date of the Conference Room"
+    }
+  };
 
 
   submittedFormData;
